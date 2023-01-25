833f93d135acc56eb5cec076068aa966e48f7c61
Tim Howe <53109226+tbhowe@users.noreply.github.com>
2023-01-25 11:25:41 +0000

    loads all annual flights csvs into dict of dfs

---
+++ flights.py
@@ -0,0 +1,11 @@
+#%%
+import pandas as pd
+
+years_list=list(range(1987, 1996+1))
+years_list.remove(1988)
+path='data-analytics-files/'
+year_dataframes={}
+for year in years_list:
+    filepath=path+str(year)+'.csv'
+    year_dataframes[str(year)]=pd.read_csv(filepath)
+# %%
